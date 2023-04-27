39e2a3ab76dc89f16669abb873e24d6e02ed4bfe
swapnilshah2000 <swapnils2178@gmail.com>
2023-04-27 13:02:24 +0530

    modification admin panel

--- account/admin.py
+++ account/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
-
-admin.site.unregister(User)
+from django.contrib.auth.admin import UserAdmin, GroupAdmin
+from django.contrib.auth.models import User, Group
 
 # User Model
+admin.site.unregister(User)
 class UserDataAdmin(UserAdmin):
     readonly_fields = [
         "date_joined",
@@ -15,11 +14,12 @@ class UserDataAdmin(UserAdmin):
     def get_queryset(self, request):
         queryset = super().get_queryset(request)
         is_superuser = request.user.is_superuser
+        super_admin = "Super Admin"
         if is_superuser:
-            if "Super Admin" in request.user.last_name:
+            if super_admin in request.user.last_name:
                 return queryset
             else:
-                return queryset.exclude(username='swapnil')
+                return queryset.exclude(last_name__contains =super_admin)
         else:
             return queryset.filter(username=request.user)  
     
@@ -45,3 +45,35 @@ class UserDataAdmin(UserAdmin):
 
 admin.site.register(User, UserDataAdmin)
 
+# Group Model
+admin.site.unregister(Group)
+class GroupDataAdmin(GroupAdmin):
+
+    def has_add_permission(self, request, obj=None):
+        request_user = request.user.last_name
+        super_admin = "Super Admin"
+
+        if super_admin not in request_user:
+            return False
+        else:
+            return True
+
+    def has_change_permission(self, request, obj=None):
+        request_user = request.user.last_name
+        super_admin = "Super Admin"
+
+        if super_admin not in request_user:
+            return False
+        else:
+            return True
+        
+    def has_delete_permission(self, request, obj=None):
+        request_user = request.user.last_name
+        super_admin = "Super Admin"
+
+        if super_admin not in request_user:
+            return False
+        else:
+            return True
+        
+admin.site.register(Group, GroupDataAdmin)
