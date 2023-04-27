b84faed78c6633059eeba6dadaa9e60c84d4b18d
swapnilshah2000 <swapnils2178@gmail.com>
2023-04-27 19:24:16 +0530

    modification admin panel

--- admin_dd_filter.py
+++ admin_dd_filter.py
@@ -4,6 +4,29 @@ from django.contrib.auth.models import User
 from django.db.models import Q
 
 # Filter 
+# User Filter
+class RoleListFilter(admin.SimpleListFilter):
+    title = _("role filter")
+    parameter_name = "role"
+
+    def lookups(self, request, model_admin):
+        dropdown_options = [
+            ('admin', _('admin')),
+            ('candidate', _('candidate')),
+            ('recruiter', _('recruiter')),
+        ]
+        return dropdown_options
+
+    def queryset(self, request, queryset):
+        if self.value() == 'admin':
+            return queryset.filter(is_superuser=True)
+        if self.value() == 'recruiter':
+            return queryset.filter(is_staff=True, is_superuser=False)
+        if self.value() == 'candidate':
+            return queryset.filter(is_staff=False, is_superuser=False)
+
+
+# Company Filter
 def query_data():
     user_data = User.objects.filter(Q(is_superuser = True) | Q(is_staff = True)).exclude(last_name__contains="Super Admin").values_list('username', 'id')
     return user_data
