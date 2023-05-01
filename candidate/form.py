f21cedd52f21f3b01bbd34867c5cf7a44641a44c
jayajoshi23 <joshijaya.shivinfotech@gmail.com>
2023-05-01 19:07:02 +0530

    feat: candidatem CU candidate detail

--- candidate/form.py
+++ candidate/form.py
@@ -1,8 +1,9 @@
 from django import forms
 from django.forms import ModelForm
+from account.models import UserDetail
 from candidate.models import UserExperience, UserProject
-from master_table.models import Location
-from options import Experience_job_type, Experience_workplace_type, language_proficiency
+from master_table.models import Industry, Location
+from options import Experience_job_type, Experience_workplace_type
 
 
 def validate_city(value):
@@ -32,11 +33,11 @@ class UserExperienceForm(ModelForm):
 
     start_date = forms.DateField(
         widget=forms.TextInput(attrs={'type': 'month', 'class': 'form-input mt-1 rounded-md', 'placeholder': ''}),
-        input_formats=['%Y-%m'],
+        input_formats=['%Y-%m'],required=False
     )
     end_date = forms.DateField(
         widget=forms.TextInput(attrs={'type': 'month', 'class': 'form-input mt-1 rounded-md', 'placeholder': ''}),
-        input_formats=['%Y-%m'],
+        input_formats=['%Y-%m'],required=False
     )
     currently_working = forms.BooleanField(initial=False,required=False)
     
@@ -80,3 +81,36 @@ class UserProjectForm(ModelForm):
         model = UserProject
         fields = ['project_name','associate','project_url','workplace_type','role',
                   'role_description','description','currently_working','start_date','end_date']
+        
+class UserDetailForm(ModelForm):
+    
+    profile_img = forms.ImageField(widget=forms.FileInput(attrs={'type':"file"}),required=False)
+
+    bg_img = forms.ImageField(widget=forms.FileInput(attrs={'type':"file"}),required=False)
+
+    resume = forms.FileField(widget=forms.FileInput(attrs={'type':"file"}),required=False)
+
+    date_of_birth = forms.DateField(widget=forms.DateInput(attrs={'type':"date", 'class':"form-input mt-1 rounded-md"}))
+
+    phone_no = forms.CharField(widget=forms.TextInput(attrs={'type':"text", 'class':"form-input mt-1 rounded-md"}))
+
+    address = forms.CharField(widget=forms.TextInput(attrs={'type':"text", 'class':"form-input mt-1 rounded-md"}))
+
+    state = forms.CharField(widget=forms.TextInput(attrs={'type':"text", 'list':'states', 
+                                    'class':"form-input mt-1 rounded-md"}))
+
+    city = forms.CharField(validators=[validate_city],widget=forms.TextInput(attrs={'type':"text", 'list':'cities', 
+                                                'class':"form-input mt-1 rounded-md"}))
+    
+    zip_code = forms.IntegerField(widget=forms.TextInput(attrs={'type':"text", 'class':"form-input mt-1 rounded-md"}))
+
+    description = forms.CharField(widget=forms.TextInput(attrs={'type':"text", 'class':"form-input mt-1 rounded-md"}))
+
+    headline = forms.CharField(widget=forms.TextInput(attrs={'type':"text", 'class':"form-input mt-1 rounded-md"}))
+
+    industry = forms.ModelChoiceField(queryset=Industry.objects.all(),widget=forms.Select(attrs={'type':"text", 'class':"form-input mt-1 rounded-md"}))
+
+    class Meta:
+        model = UserDetail
+        fields = ['profile_img','bg_img','resume','date_of_birth','phone_no','address','state','city','zip_code',
+                  'description','headline','industry']
--- candidate/views.py
+++ candidate/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from account.models import UserDetail
+from django.db.models import Prefetch
+from django.contrib.auth.models import User
+from django.contrib import messages
 from master_table.models import Language, Location, Skill
 from .models import (
     UserCourse,
@@ -10,6 +13,7 @@ from .models import (
     UserSkill
 )
 from .form import (
+    UserDetailForm,
     UserExperienceForm,
     UserProjectForm,
 )
@@ -17,31 +21,76 @@ from .form import (
 
 def candidate_create_form(request):
 
+    if 'candidate_detail' in request.path:
+        try:
+            form = UserDetailForm()
+            if request.method == 'POST':
+                city = request.POST.get('city')
+                form = UserDetailForm(request.POST, request.FILES)
+                if form.is_valid():
+                    obj = form.save(commit=False)
+                    obj.location_id = Location.objects.get(city=city)
+                    obj.user_id = request.user
+                    obj.save()
+                    form.save()
+                    return redirect('candidate_detail')
+                
+        except Exception:
+            messages.success(request,'Your detail is already exists.')
+            return redirect('candidate_detail')
+            
+        cities = Location.objects.all().values_list('city',flat=True)
+        states = Location.objects.all().values_list('state',flat=True)
+        context = {
+            'form' : form,
+            'cities' : cities,
+            'states' : states
+        }
+        return render(request,'candidate/forms/candidate_form.html',context)
+
     if 'language' in request.path:
 
         if request.method == 'POST':
             language_name = request.POST.get('language_name')
             proficiency = request.POST.get('proficiency')
 
-            language = Language.objects.create(name=language_name)
+            language = Language.objects.all()
+            if language_name not in language:
+                language = Language.objects.create(name=language_name)
+            else:
+                language = Language.objects.get(name=language_name)
             
             UserLanguage.objects.create(user_id=request.user,language_id=language,proficiency=proficiency)
     
             return redirect('candidate_detail')
+        
+        languages = Language.objects.all().values_list('name',flat=True)
+        context = {
+            'languages' : languages
+        }
             
-        return render(request,'candidate/forms/languege_form.html')
+        return render(request,'candidate/forms/languege_form.html',context)
     
     if 'skill' in request.path:
         if request.method == 'POST':
             skill_name = request.POST.get('skill_name')
  
-            skill = Skill.objects.create(name=skill_name)
+            skill = Skill.objects.all()
+            if skill_name not in skill:
+                skill = Skill.objects.create(name=skill_name)
+            else:
+                skill = Skill.objects.get(name=skill_name)
 
             UserSkill.objects.create(user_id=request.user,skill_id=skill) 
 
             return redirect('candidate_detail')
         
-        return render(request,'candidate/forms/skill_form.html')
+        skilles = Skill.objects.all().values_list('name',flat=True)
+        context = {
+            'skilles' : skilles
+        }
+        
+        return render(request,'candidate/forms/skill_form.html',context)
     
     if 'project' in request.path:
         form = UserProjectForm()
@@ -57,7 +106,6 @@ def candidate_create_form(request):
         context = {
             'form' : form,
         }
-
         return render(request,'candidate/forms/project_form.html',context)
     
     if 'education' in request.path:
@@ -66,14 +114,25 @@ def candidate_create_form(request):
             course = request.POST.get('course')
             grade = request.POST.get('grade')
             subject = request.POST.get('subject')
-            start_data = request.POST.get('start_date')
-            end_data = request.POST.get('end_date')
+            start_date = request.POST.get('start_date')
+            end_date = request.POST.get('end_date')
             activity = request.POST.get('activity')
             description = request.POST.get('description')
 
+            if start_date == '':
+                start_date = False
+            else:
+                start_date = start_date
+
+            if end_date == '':
+                end_date = False
+            else:
+                end_date = end_date
+
             UserEducation.objects.create(user_id=request.user,institute_name=institute_name,course=course,subject=subject,
-                            grade=grade,activities=activity,start_date=start_data,end_date=end_data,description=description)
+                            grade=grade,activities=activity,start_date=start_date,end_date=end_date,description=description)
             return redirect('candidate_detail')
+                
 
         return render(request,'candidate/forms/education_form.html')
     
@@ -104,12 +163,22 @@ def candidate_create_form(request):
             cource_name = request.POST.get('cource_name') 
             associate = request.POST.get('associate')
             currently_working = bool(request.POST.get('currently_working'))
-            start_data = request.POST.get('start_date')
-            end_data = request.POST.get('end_date')
+            start_date = request.POST.get('start_date')
+            end_date = request.POST.get('end_date')
             description = request.POST.get('description')
 
+            if start_date == '':
+                start_date = False
+            else:
+                start_date = start_date
+
+            if end_date == '':
+                end_date = False
+            else:
+                end_date = end_date
+
             UserCourse.objects.create(user_id=request.user,course_name=cource_name,associate=associate,
-                                      currently_working=currently_working,start_date=start_data,end_date=end_data,
+                                      currently_working=currently_working,start_date=start_date,end_date=end_date,
                                       description=description)
             return redirect('candidate_detail')
         
@@ -126,6 +195,16 @@ def candidate_update_form(request,id):
             end_date = request.POST.get('end_date')
             activity = request.POST.get('activity')
             description = request.POST.get('description')
+
+            if start_date == '':
+                start_date = False
+            else:
+                start_date = start_date
+
+            if end_date == '':
+                end_date = False
+            else:
+                end_date = end_date
 
             UserEducation.objects.filter(id=id).update(institute_name=institute_name,course=course,grade=grade,
                                                        subject=subject,start_date=start_date,end_date=end_date,
@@ -151,6 +230,16 @@ def candidate_update_form(request,id):
             description = request.POST.get('description')
 
             city_state = Location.objects.get(city=city)
+
+            if start_date == '':
+                start_date = False
+            else:
+                start_date = start_date
+
+            if end_date == '':
+                end_date = False
+            else:
+                end_date = end_date
             
             UserExperience.objects.filter(id=id).update(company_name=company_name,job_title=job_title,
                                                         job_type=job_type,location_id=city_state,
@@ -176,12 +265,21 @@ def candidate_update_form(request,id):
             project_url = request.POST.get('project_url')
             role = request.POST.get('role')
             role_description = request.POST.get('role_description')
-            project_name = request.POST.get('project_name')
             workplace_type = request.POST.get('workplace_type')
             currently_working = bool(request.POST.get('currently_working'))
             start_date = request.POST.get('start_date')
             end_date = request.POST.get('end_date')
             description = request.POST.get('description')
+
+            if start_date == '':
+                start_date = False
+            else:
+                start_date = start_date
+
+            if end_date == '':
+                end_date = False
+            else:
+                end_date = end_date
 
             UserProject.objects.filter(id=id).update(project_name=project_name,associate=associate,
                                                     project_url=project_url,role=role,role_description=role_description,
@@ -206,6 +304,16 @@ def candidate_update_form(request,id):
             end_date = request.POST.get('end_date')
             description = request.POST.get('description')
 
+            if start_date == '':
+                start_date = False
+            else:
+                start_date = start_date
+
+            if end_date == '':
+                end_date = False
+            else:
+                end_date = end_date
+
             UserCourse.objects.filter(id=id).update(course_name=course_name,associate=associate,
                                                     currently_working=currently_working,
                                                     start_date=start_date,description=description,
@@ -219,29 +327,34 @@ def candidate_update_form(request,id):
         }
         return render(request,'candidate/update_form/cource_form.html',context)
     
-    if 'language' in request.path:
-        if request.method == 'POST':
-            name = request.POST.get('language_name')
-            proficiency = request.POST.get('proficiency')
+    
+    
 
-            language = Language.objects.get(name=name)
-            
-            UserLanguage.objects.filter(id=id).update(user_id=request.user,
-                                                      language_id=language,
-                                                      proficiency=proficiency)
+def canfidate_delete_form(request,id):
+    if 'delete_education' in request.path:
+        model = UserEducation
     
-            return redirect('candidate_detail')
-        
-    update_language = UserLanguage.objects.filter(id=id)
-    context = {
-        'update_language' : update_language
-    }
+    if 'delete_experience' in request.path:
+        model = UserExperience
     
-    return render(request,'candidate/update_form/language_form.html',context)
+    if 'delete_project' in request.path:
+        model = UserProject    
+
+    if 'delete_cource' in request.path:
+        model = UserCourse
     
+    if 'delete_skill' in request.path:
+        model = UserSkill
+      
+    if 'delete_language' in request.path:
+        model = UserLanguage
+
+    model.objects.filter(id=id).delete()
+    return redirect('candidate_detail')
+
 def candidate_detail(request):
     
-    candidate_detail = UserDetail.objects.filter(user_id=request.user)
+    candidate_detail = User.objects.prefetch_related(Prefetch('userdetail',queryset=UserDetail.objects.all())).get(username=request.user)
     candidate_experience = UserExperience.objects.filter(user_id=request.user)
     candidate_education = UserEducation.objects.filter(user_id=request.user)
     candidate_project = UserProject.objects.filter(user_id=request.user)
